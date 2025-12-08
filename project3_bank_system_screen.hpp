5873817891a127e1b907934307fc0f28137fa1ce
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-08 15:33:39 +0100

    screen base class header file

---
+++ project3_bank_system_screen.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "project3_bank_system_client.hpp"
+
+class clsScreen
+{
+protected :
+    static void _DrawScreenHeader(string Title,string SubTitle ="")
+    {
+        cout << "\t\t\t\t\t______________________________________";
+        cout << "\n\n\t\t\t\t\t  " << Title;
+        if (SubTitle != "")
+        {
+            cout << "\n\t\t\t\t\t  " << SubTitle;
+        }
+        cout << "\n\t\t\t\t\t______________________________________\n\n";
+    }
+
+
+};
+
