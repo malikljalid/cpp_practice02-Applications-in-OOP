1dafcc5bb23d9ae77e7a866bcec4924de82165ef
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-13 00:05:21 +0100

    screen client liss updated class

--- project3_bank_system_screenClientList.hpp
+++ project3_bank_system_screenClientList.hpp
@@ -24,7 +24,10 @@ public:
 
    static void ShowClientsList()
     {
-
+        if (!CheckAccessRights(enPermissions::pListClients))
+        {
+            return;// this will exit the function and it will not continue
+        }
         
         vector <clsBankClient> vClients = clsBankClient::GetClientsList();
         string Title = "\t  Client List Screen";
