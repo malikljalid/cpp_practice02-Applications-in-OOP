0f3b5eb2bea85ddadfdbc08e289a2b2cf07ceff7
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-13 14:29:22 +0100

    new update on login screen class:

--- project3_bank_system_screenLogin.hpp
+++ project3_bank_system_screenLogin.hpp
@@ -8,17 +8,28 @@ class clsLoginScreen :protected clsScreen
 
 private :
 
-  static  void _Login()
+    static  bool _Login()
     {
         bool LoginFaild = false;
+        short FaildLoginCount = 0;
 
         string Username, Password;
         do
         {
-     
+
             if (LoginFaild)
             {
-                cout << "\nInvlaid Username/Password!\n\n";
+                FaildLoginCount++;
+
+                cout << "\nInvlaid Username/Password!";
+                cout << "\nYou have " << (3-FaildLoginCount)
+                     << " Trial(s) to login.\n\n";   
+            }
+
+            if (FaildLoginCount == 3)
+            {
+                cout << "\nYour are Locked after 3 faild trails \n\n";
+                return false;
             }
 
             cout << "Enter Username? ";
@@ -34,17 +45,17 @@ private :
         } while (LoginFaild);
 
         clsMainScreen::ShowMainMenue();
-
+        return true;
     }
 
 public:
 
 
-    static void ShowLoginScreen()
+    static bool ShowLoginScreen()
     {
         system("cls");
         _DrawScreenHeader("\t  Login Screen");
-        _Login();
+        return (_Login());
 
     }
 
