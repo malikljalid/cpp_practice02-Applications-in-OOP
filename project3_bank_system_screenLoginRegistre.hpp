dbd64f28d01a31663ccee829d9b68e0e1af7be7a
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-13 17:58:49 +0100

    login of screen registre added to the project

---
+++ project3_bank_system_screenLoginRegistre.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "project3_bank_system_screen.hpp"
+#include "project3_bank_system_user.hpp"
+
+class clsLoginRegisterScreen :protected clsScreen
+{
+
+private:
+    
+    static void PrintLoginRegisterRecordLine(stLoginRegisterRecord LoginRegisterRecord)
+    {
+
+        cout << setw(8) << left << "" << "| " << setw(35) << left << LoginRegisterRecord.DateTime;
+        cout << "| " << setw(20) << left << LoginRegisterRecord.UserName;
+        cout << "| " << setw(20) << left << LoginRegisterRecord.Password;
+        cout << "| " << setw(10) << left << LoginRegisterRecord.Permissions;
+    }
+
+public:
+  
+    static void ShowLoginRegisterScreen()
+    {
+
+        vector <stLoginRegisterRecord> vLoginRegisterRecord = clsUser::GetLoginRegisterList();
+  
+        string Title = "\tLogin Register List Screen";
+        string SubTitle =  "\t\t(" + to_string(vLoginRegisterRecord.size()) + ") Record(s).";
+
+        _DrawScreenHeader(Title, SubTitle);
+
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+        cout << setw(8) << left << "" << "| " << left << setw(35) << "Date/Time";
+        cout << "| " << left << setw(20) << "UserName";
+        cout << "| " << left << setw(20) << "Password";
+        cout << "| " << left << setw(10) << "Permissions";
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+        if (vLoginRegisterRecord.size() == 0)
+            cout << "\t\t\t\tNo Logins Available In the System!";
+        else
+
+            for (stLoginRegisterRecord Record : vLoginRegisterRecord)
+            {
+
+                PrintLoginRegisterRecordLine(Record);
+                cout << endl;
+            }
+
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+    }
+
+};
+
