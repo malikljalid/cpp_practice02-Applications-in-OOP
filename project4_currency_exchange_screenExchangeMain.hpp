0bc96bc26df7609c5b0f3de05b5e6adad2387235
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-12-14 21:30:55 +0100

    add update currency section

--- project4_currency_exchange_screenExchangeMain.hpp
+++ project4_currency_exchange_screenExchangeMain.hpp
@@ -43,7 +43,7 @@ private:
     static void _ShowUpdateCurrencyRateScreen()
     {
         // cout << "\nUpdate Currency Rate Screen Will Be Here.\n";
-        // clsUpdateCurrencyRateScreen::ShowUpdateCurrencyRateScreen();
+        clsUpdateCurrencyRateScreen::ShowUpdateCurrencyRateScreen();
     }
 
     static void _ShowCurrencyCalculatorScreen()
